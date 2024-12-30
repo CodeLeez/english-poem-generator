94531e56fa9b8b937fa9db34563ecb11ee17d958
Elizabeth Chigudu <elizabethchigudu@gmail.com>
2024-12-30 12:52:36 +0800

    changed max lines of poem

--- src/script.js
+++ src/script.js
@@ -12,7 +12,7 @@ function generatePoem(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "c9057t5643d1ef0070e941b73ao8bb3f";
-  let prompt = `Generate an English poem about ${instructionsInput.value} The poem should have a maximum of 10 lines and Separate each line with a <br/> Do not write a title no greetings please just start the poem and Always end the poem by Signing with ElizabethChigudu AI Poems inside a <strong> element`;
+  let prompt = `Generate an English poem about ${instructionsInput.value} The poem should have a maximum of 7 lines and Separate each line with a <br/> Do not write a title no greetings please just start the poem and Always end the poem by Signing with ElizabethChigudu AI Poems inside a <strong> element`;
   let context = "";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 

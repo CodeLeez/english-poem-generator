1d55e75f1263ae6bc274c8e64574b91cd2dd001f
Elizabeth Chigudu <elizabethchigudu@gmail.com>
2024-12-30 11:30:20 +0800

    Added AI API Integration

--- src/script.js
+++ src/script.js
@@ -1,12 +1,22 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "This is my sample poem.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "c9057t5643d1ef0070e941b73ao8bb3f";
+  let prompt = `Generate an English poem about ${instructionsInput.value} Separate each line with a <br/> Do not write a title no greetings please just start the poem and Always end the poem by Signing with ElizabethChigudu AI Poems inside a <strong> element`;
+  let context = "";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
